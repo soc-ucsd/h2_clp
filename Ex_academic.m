84bfb4828b9b3582dc176eef1702fa1329d934c5
Yang Zheng <zhengy093@gmail.com>
2020-03-05 15:41:39 -0500

    Created three test examples

---
+++ Ex_academic.m
@@ -0,0 +1,123 @@
+
+%% Academic example
+
+
+n = 2;  % number of states
+p = 1;  % number of outputs
+m = 1;  % number of inputs
+r = p;  % number of disturbances
+
+Q = 1*eye(p);
+R = 1*eye(m);
+
+% Generalized state-space model
+% x[t+1] = A x[t] + B1 w[t] + B2  u[t]
+% z[t]   = C1x[t] + D11w[t] + D12 u[t]
+% y[t]   = C2x[t] + D21w[t] + D22 u[t]
+
+dT = 0.1;  % discrete time interval
+
+A = [1 2; 3 2];
+B2 = [0;1];
+C2 = [1 0];
+
+B1 = zeros(n,r); 
+D21 = eye(p);     D22 = zeros(p,m);
+C1  = [Q^(0.5)*C2;
+        zeros(m,n)];
+D11 = [Q^(0.5);
+        zeros(m,p)];
+D12 = [zeros(p,m);
+        R^(0.5)];
+%  generalized state-space model
+B = [B1, B2];
+C = [C1;C2];
+D = [D11 D12;
+     D21 D22];
+
+P = ss(A,B,C,D,dT);  % discrete time model -- transfer matrices
+
+%% standard H2 control
+[K,CL,gamma,info] = h2syn(P,p,m);  % there is some issue with this formulation! 2020/03/04
+
+
+%% Closed-loop optimization
+opts.N  = 15;
+opts.solver  = 'mosek';
+%    SLS
+opts.type    = 1;
+[Ksls,H2sls,infosls] = clph2(A,B2,C2,Q,R,opts);
+
+% IOP
+opts.type    = 2;
+[Kiop,H2iop,infoiop] = clph2(A,B2,C2,Q,R,opts);
+
+%% time-domain simulaiton
+close all
+
+eps = 10^(-6);
+Kiopr = minreal(Kiop,eps);
+Kslsr = minreal(Ksls,eps);
+
+
+G = ss(A,B2,C2,D22,dT);
+
+T = 5;
+Tn = floor(T/dT);   % number of iterations
+dx = zeros(n,Tn);
+dy = zeros(p,Tn);
+du = zeros(m,Tn);
+
+x0 = rand(n,1);  % initial disturbance
+%dy(:,1) = 10*rand(p,1);  % initial measurement noise
+
+%% SLS
+[x,y,u,kesi] = dynsim(G,Ksls,dT,dx,dy,du,T,x0);
+Time = (1:Tn)*dT;
+figure; subplot(2,1,1); plot(Time,u); xlabel('time (s)'); ylabel('Control input'); title('SLS')
+        subplot(2,1,2); plot(Time,max(abs(kesi))); xlabel('time (s)'); ylabel('internal state');
+figure; 
+subplot(2,1,1); plot(Time,x(1,:)); xlabel('time (s)'); ylabel('x_1'); title('SLS')
+subplot(2,1,2); plot(Time,x(2,:)); xlabel('time (s)'); ylabel('x_2');
+
+% SLSr
+[x,y,u,kesi] = dynsim(G,Kslsr,dT,dx,dy,du,T,x0);
+
+Time = (1:Tn)*dT;
+figure; subplot(2,1,1); plot(Time,u); xlabel('time (s)'); ylabel('Control input'); title('SLS reduction')
+        subplot(2,1,2); plot(Time,max(abs(kesi))); xlabel('time (s)'); ylabel('internal state');
+figure; 
+subplot(2,1,1); plot(Time,x(1,:)); xlabel('time (s)'); ylabel('x_1'); title('SLS reduction')
+subplot(2,1,2); plot(Time,x(2,:)); xlabel('time (s)'); ylabel('x_2');
+
+%% IOP
+[x,y,u,kesi] = dynsim(G,Kiop,dT,dx,dy,du,T,x0);
+Time = (1:Tn)*dT;
+figure; subplot(2,1,1); plot(Time,u); xlabel('time (s)'); ylabel('Control input'); title('IOP')
+        subplot(2,1,2); plot(Time,max(abs(kesi))); xlabel('time (s)'); ylabel('internal state');
+figure; 
+subplot(2,1,1); plot(Time,x(1,:)); xlabel('time (s)'); ylabel('x_1'); title('IOP')
+subplot(2,1,2); plot(Time,x(2,:)); xlabel('time (s)'); ylabel('x_2');
+
+% IOPr
+[x,y,u,kesi] = dynsim(G,Kiopr,dT,dx,dy,du,T,x0);
+Time = (1:Tn)*dT;
+figure; subplot(2,1,1); plot(Time,u); xlabel('time (s)'); ylabel('Control input'); title('IOP reduction')
+        subplot(2,1,2); plot(Time,max(abs(kesi))); xlabel('time (s)'); ylabel('internal state');
+figure; 
+subplot(2,1,1); plot(Time,x(1,:)); xlabel('time (s)'); ylabel('x_1'); title('IOP reduction')
+subplot(2,1,2); plot(Time,x(2,:)); xlabel('time (s)'); ylabel('x_2');
+
+%% H2 optimal controller  -- this behavior is very strange
+% [x,y,u,kesi] = dynsim(G,K,dT,dx,dy,du,T,x0);
+% Time = (1:Tn)*dT;
+% figure; subplot(2,1,1); plot(Time,u); xlabel('time (s)'); ylabel('Control input'); title('IOP reduction')
+%            subplot(2,1,2); plot(Time,max(abs(kesi))); xlabel('time (s)'); ylabel('internal state');
+% figure; 
+% subplot(2,1,1); plot(Time,x(1,:)); xlabel('time (s)'); ylabel('x_1'); title('H2 optimal controller')
+% subplot(2,1,2); plot(Time,x(2,:)); xlabel('time (s)'); ylabel('x_2');
+
+
+
+
+
---
+++ Ex_car_following.m
@@ -0,0 +1,158 @@
+% Example 1: Car-following example
+
+
+clc; clear all; 
+clear yalmip
+
+% dynamics in discrete time
+% Generalized state-space model
+% x[t+1] = A x[t] + B1 w[t] + B2  u[t]
+% z[t]   = C1x[t] + D11w[t] + D12 u[t]
+% y[t]   = C2x[t] + D21w[t] + D22 u[t]
+
+
+n = 3;  % number of states
+p = 2;  % number of outputs
+m = 1;  % number of inputs
+r = p;  % number of disturbances
+
+Q = 1*eye(p);
+R = 1*eye(m);
+
+tau    = 0.5;    % tau in the dynamics
+deltaT = 0.1;    % discrete time interval
+
+A   = eye(n) + [0 1 0;
+                0 0 1;
+                0 0 -1/tau]*deltaT; 
+B2  = [0;0;1/tau]*deltaT;   
+C2  = [1 0 0;
+       0 1 0]; 
+
+B1 = zeros(n,r); 
+D21 = eye(p);     D22 = zeros(p,m);
+C1  = [Q^(0.5)*C2;
+        zeros(m,n)];
+D11 = [Q^(0.5);
+        zeros(m,p)];
+D12 = [zeros(p,m);
+        R^(0.5)];
+
+%  generalized state-space model
+B = [B1, B2];
+C = [C1;C2];
+D = [D11 D12;
+     D21 D22];
+
+P = ss(A,B,C,D,deltaT);  % discrete time model -- transfer matrices
+
+%% standard H2 control
+[K,CL,gamma,info] = h2syn(P,p,m);
+
+%% H2 synthesis via Closed-loop parameterization
+
+N = 60; %:10:50;
+%N = [N,75,100];
+
+H2optimal = zeros(length(N),4);
+
+for k = 1:length(N)
+
+    opts.N       = N(k);
+    opts.solver  = 'mosek';
+%    SLS
+    opts.type    = 1;
+   [Ksls,H2sls,infosls] = clph2(A,B2,C2,Q,R,opts);
+
+    % IOP
+    opts.type    = 2;
+    [Kiop,H2iop,infoiop] = clph2(A,B2,C2,Q,R,opts);
+    
+    % Type 3 -- mixed sls/iop
+   % opts.type    = 3;
+   % [Kty3,H2ty3,info3] = clph2(A,B2,C2,Q,R,opts);
+    
+    % Type 4 -- mixed sls/iop
+   % opts.type    = 4;
+   % [Kty4,H2ty4,info4] = clph2(A,B2,C2,Q,R,opts);
+    
+   % H2optimal(k,:) = [H2iop, H2sls, H2ty3, H2ty4];
+   %  H2optimal(k,:) = [0, 0, 0, H2ty4];
+
+end
+
+
+%% time-domain simulaiton
+eps = 10^(-6);
+Kiopr = minreal(Kiop,eps);
+Kslsr = minreal(Ksls,eps);
+
+
+G = ss(A,B2,C2,D22,deltaT);
+
+T = 10;
+Tn = floor(T/deltaT);   % number of iterations
+dx = zeros(n,Tn);
+dy = zeros(p,Tn);
+du = zeros(m,Tn);
+
+x0 = 5*rand(n,1);  % initial disturbance
+%dy(:,1) = 10*rand(p,1);  % initial measurement noise
+
+%% IOP
+[x,y,u,kesi] = dynsim(G,Kiop,deltaT,dx,dy,du,T,x0);
+
+% figure
+close all
+Time = (1:Tn)*deltaT;
+figure; subplot(2,1,1); plot(Time,u); xlabel('time (s)'); ylabel('Control input'); title('IOP')
+           subplot(2,1,2); plot(Time,max(abs(kesi))); xlabel('time (s)'); ylabel('internal state');
+figure;  
+subplot(3,1,1); plot(Time,x(1,:)); xlabel('time (s)'); ylabel('Spacing error'); title('IOP')
+subplot(3,1,2); plot(Time,x(2,:)); xlabel('time (s)'); ylabel('Velocity error');
+subplot(3,1,3); plot(Time,x(3,:)); xlabel('time (s)'); ylabel('Acceleration');
+
+% IOP reduction
+[x,y,u,kesi] = dynsim(G,Kiopr,deltaT,dx,dy,du,T,x0);
+
+figure; subplot(2,1,1); plot(Time,u); xlabel('time (s)'); ylabel('Control input'); title('IOP reduction')
+           subplot(2,1,2); plot(Time,max(abs(kesi))); xlabel('time (s)'); ylabel('internal state');
+figure;  
+subplot(3,1,1); plot(Time,x(1,:)); xlabel('time (s)'); ylabel('Spacing error'); title('IOP reduction')
+subplot(3,1,2); plot(Time,x(2,:)); xlabel('time (s)'); ylabel('Velocity error');
+subplot(3,1,3); plot(Time,x(3,:)); xlabel('time (s)'); ylabel('Acceleration');
+
+
+%% SLS
+% figure
+[x,y,u,kesi] = dynsim(G,Ksls,deltaT,dx,dy,du,T,x0);
+
+figure; subplot(2,1,1); plot(Time,u); xlabel('time (s)'); ylabel('Control input'); title('SLS')
+           subplot(2,1,2); plot(Time,max(abs(kesi))); xlabel('time (s)'); ylabel('internal state');
+figure; 
+subplot(3,1,1); plot(Time,x(1,:)); xlabel('time (s)'); ylabel('Spacing error');  title('SLS')
+subplot(3,1,2); plot(Time,x(2,:)); xlabel('time (s)'); ylabel('Velocity error');
+subplot(3,1,3); plot(Time,x(3,:)); xlabel('time (s)'); ylabel('Acceleration');
+
+% sls reduction
+[x,y,u,kesi] = dynsim(G,Kslsr,deltaT,dx,dy,du,T,x0);
+
+figure; subplot(2,1,1); plot(Time,u); xlabel('time (s)'); ylabel('Control input'); title('SLS reduction')
+           subplot(2,1,2); plot(Time,max(abs(kesi))); xlabel('time (s)'); ylabel('internal state');
+figure;  
+subplot(3,1,1); plot(Time,x(1,:)); xlabel('time (s)'); ylabel('Spacing error'); title('SLS reduction')
+subplot(3,1,2); plot(Time,x(2,:)); xlabel('time (s)'); ylabel('Velocity error');
+subplot(3,1,3); plot(Time,x(3,:)); xlabel('time (s)'); ylabel('Acceleration');
+
+
+%% Optimal controller   -- this behavior is very strange -- couldn't understand
+% 
+% [x,y,u] = dynsim(G,K,deltaT,dx,dy,du,T,x0);
+% 
+% % figure
+% Time = (1:Tn)*deltaT;
+% figure(3); plot(Time,u); xlabel('time (s)'); ylabel('Control input');
+% figure(4); 
+% subplot(3,1,1); plot(Time,x(1,:)); xlabel('time (s)'); ylabel('Spacing error');
+% subplot(3,1,2); plot(Time,x(2,:)); xlabel('time (s)'); ylabel('Velocity error');
+% subplot(3,1,3); plot(Time,x(3,:)); xlabel('time (s)'); ylabel('Acceleration');
